dcbd4f96e9b41ac9c3af6f3770f387e3e714c08c
Sam Mckay <sammckay12@gmail.com>
2017-01-17 09:37:21 +0000

    using forms

--- app.rb
+++ app.rb
@@ -32,6 +32,12 @@ get '/random-cat' do
   erb(:index)
 end
 
+post '/named-cat' do
+  p params
+  @name = params[:name]
+  erb(:index)
+end
+
 get '/named-cat' do
   p params
   @name = params[:name]
